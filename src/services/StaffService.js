d2daf8cb222c41f00217d9c4990cb966e34f894e
Raymundo <raymundomc0205@icloud.com>
2026-04-22 01:28:58 -0400

    fixed errors

--- src/services/StaffService.js
+++ src/services/StaffService.js
@@ -1,45 +1,37 @@
-import {
-    getAll,
-    getById,
-    create,
-    update,
-    remove
-} from '../repositories/StaffRepo.js';
+import { getAll, getById, create, update, remove } from '../repositories/StaffRepo.js';
 
 export async function getAllStaff(options) {
-    return getAll(options);
+  return getAll(options);
 }
 
 export async function getStaffById(id) {
-    const staff = await getById(id)
+  const staff = await getById(id);
 
-    if(staff) return staff;
+  if (staff) return staff;
 
-    const error = new Error(`Staff ${id} not found`);
-    error.status = 404;
-    throw error;
+  const error = new Error(`Staff ${id} not found`);
+  error.status = 404;
+  throw error;
 }
 
 export async function createStaff(staffData) {
-    return create(staffData);
+  return create(staffData);
 }
 
 export async function updatedStaff(id, updatedStaffData) {
-    const updated_staff_data = await update(id, updatedStaffData);
-    if(updated_staff_data) return updated_staff_data;
-    else{
-        const error = new Error(`Staff ${id} not found`);
-        error.status = 404;
-        throw error;
-    }
+  const updated = await update(id, updatedStaffData);
+  if (updated) return updated;
+
+  const error = new Error(`Staff ${id} not found`);
+  error.status = 404;
+  throw error;
 }
 
 export async function deleteStaff(id) {
-    const result = await remove(id);
-    if(result) return;
-    else {
-        const error = new Error(`Staff ${id} not found`);
-        error.status = 404;
-        throw error;
-    }
+  const result = await remove(id);
+  if (result) return;
+
+  const error = new Error(`Staff ${id} not found`);
+  error.status = 404;
+  throw error;
 }
