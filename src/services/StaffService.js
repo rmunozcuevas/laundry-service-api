e9f599d0efd7eab6efd84fa73ab791427f34853c
Raymundo <raymundomc0205@icloud.com>
2026-04-22 01:08:48 -0400

    added both Read Functions

--- src/services/StaffService.js
+++ src/services/StaffService.js
@@ -6,3 +6,16 @@ import {
     remove
 } from '../repositories/StaffRepo.js';
 
+export async function getAllStaff(options) {
+    const staff = await getAll(options);
+}
+
+export async function getStaffById(id) {
+    const staff = await getById(id)
+
+    if(staff) return staff;
+
+    const error = new Error(`Staff ${id} not found`);
+    error.status = 404;
+    throw error;
+}
